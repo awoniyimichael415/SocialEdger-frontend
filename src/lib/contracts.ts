1652ede54de2472954c8fdecb7259f1731c09345
awoniyimichael415 <awoniyimichael415@gmail.com>
2026-07-20 12:51:25 -0600

    Update contracts.ts

--- src/lib/contracts.ts
+++ src/lib/contracts.ts
@@ -1,7 +1,58 @@
 import MembershipNFT from "@/src/abi/MembershipNFT.json";
 import Presale from "@/src/abi/Presale.json";
 import PresaleToken from "@/src/abi/PresaleToken.json";
+import DAOGovernance from "@/src/abi/DAOGovernance.json";import type { Abi } from "viem";
+
+import MembershipNFT from "@/src/abi/MembershipNFT.json";
+import Presale from "@/src/abi/Presale.json";
+import PresaleToken from "@/src/abi/PresaleToken.json";
 import DAOGovernance from "@/src/abi/DAOGovernance.json";
+
+export const CONTRACTS = {
+  /*
+  =====================================
+  MEMBERSHIP NFT
+  =====================================
+  */
+  MembershipNFT: {
+    address:
+      "0xd906Ca726EbaB12c6ad627d12d70F4c5a8807922" as const,
+    abi: MembershipNFT.abi as Abi,
+  },
+
+  /*
+  =====================================
+  SET TOKEN
+  =====================================
+  */
+  PresaleToken: {
+    address:
+      "0x0d81FF96550C692A77755B24C4a5bF86B6c9519A" as const,
+    abi: PresaleToken.abi as Abi,
+  },
+
+  /*
+  =====================================
+  PRESALE
+  =====================================
+  */
+  Presale: {
+    address:
+      "0x967eEdb35704E692fe106b333c2d32479fbB11B7" as const,
+    abi: Presale.abi as Abi,
+  },
+
+  /*
+  =====================================
+  DAO GOVERNANCE
+  =====================================
+  */
+  DAOGovernance: {
+    address:
+      "0xeDE65Cd7Cfb622B8d299A6982A77d514080dAf1A" as const,
+    abi: DAOGovernance.abi as Abi,
+  },
+} as const;
 
 export const CONTRACTS = {
   /*
